ab9f89346ee644b578f2a3ad483ae43bf9a5a834
bn65bullock <82971038+bn65bullock@users.noreply.github.com>
2021-05-14 03:44:17 -0400

    Election analysis with counties added

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -6,20 +6,20 @@ import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("..", "Resources", "election_results.csv")
+file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
 
-# Candidate Options and candidate votes.
+# Candidate Options and candidate votes
 candidate_options = []
 candidate_votes = {}
 
 # 1: Create a county list and county votes dictionary.
-
-
+county_names = []
+county_votes = {}
 
 # Track the winning candidate, vote count and percentage
 winning_candidate = ""
@@ -27,8 +27,8 @@ winning_count = 0
 winning_percentage = 0
 
 # 2: Track the largest county and county voter turnout.
-
-
+largest_countyname = ""
+largest_votecount = 0
 
 # Read the csv and convert it into a list of dictionaries
 with open(file_to_load) as election_data:
@@ -47,7 +47,7 @@ with open(file_to_load) as election_data:
         candidate_name = row[2]
 
         # 3: Extract the county name from each row.
-
+        county_one_name = row[1]
 
         # If the candidate does not match any existing candidate add it to
         # the candidate list
@@ -64,16 +64,16 @@ with open(file_to_load) as election_data:
 
         # 4a: Write an if statement that checks that the
         # county does not match any existing county in the county list.
-
+        if county_one_name not in county_names:
 
             # 4b: Add the existing county to the list of counties.
-
+            county_names.append(county_one_name)
 
             # 4c: Begin tracking the county's vote count.
-
+            county_votes[county_one_name] = 0
 
         # 5: Add a vote to that county's vote count.
-
+        county_votes[county_one_name] += 1
 
 
 # Save the results to our text file.
@@ -82,33 +82,44 @@ with open(file_to_save, "w") as txt_file:
     # Print the final vote count (to terminal)
     election_results = (
         f"\nElection Results\n"
-        f"-------------------------\n"
+        f"----------------------------------------\n"
         f"Total Votes: {total_votes:,}\n"
-        f"-------------------------\n\n"
+        f"----------------------------------------\n\n"
         f"County Votes:\n")
     print(election_results, end="")
 
     txt_file.write(election_results)
 
     # 6a: Write a for loop to get the county from the county dictionary.
+    for county_one_name in county_votes:
 
-        # 6b: Retrieve the county vote count.
+        # 6b: Retrieve the county vote count
+        c_votes = county_votes[county_one_name]
 
         # 6c: Calculate the percentage of votes for the county.
+        county_votepercentage = float(c_votes) / float(total_votes) * 100
 
+        # 6d: Print the county results to the terminal.
+        county_results = (f"{county_one_name}: {county_votepercentage:.1f}% ({c_votes:,})\n")
+        print(county_results)
 
-         # 6d: Print the county results to the terminal.
+        # 6e: Save the county votes to a text file.
+        txt_file.write(county_results)
 
-         # 6e: Save the county votes to a text file.
-
-         # 6f: Write an if statement to determine the winning county and get its vote count.
-
+        # 6f: Write an if statement to determine the winning county and get its vote count.
+        if(c_votes > largest_votecount):
+            largest_votecount = c_votes
+            largest_countyname = county_one_name
 
     # 7: Print the county with the largest turnout to the terminal.
-
+    largest_county_summary = (
+        f"\n----------------------------------------\n"  
+        f"Largest County Turnout: {largest_countyname}\n"
+        f"----------------------------------------\n")
+    print(largest_county_summary)
 
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(largest_county_summary)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
@@ -133,11 +144,11 @@ with open(file_to_save, "w") as txt_file:
 
     # Print the winning candidate (to terminal)
     winning_candidate_summary = (
-        f"-------------------------\n"
+        f"----------------------------------------\n"
         f"Winner: {winning_candidate}\n"
         f"Winning Vote Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
-        f"-------------------------\n")
+        f"----------------------------------------\n")
     print(winning_candidate_summary)
 
     # Save the winning candidate's name to the text file
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# PyPoll
-Analysis of Voter turn out by county for a recent election
+# PyPoll Election Analysis
+
+## Project Overview
+Complete an election audit for a recent election and provide the results as follows.
+
+1. Calculate the total number of votes
+2. Get a complete list of Counties that voted
+3. Calculate the number of voters by County and their percentages
+4. Identify the County with the largest turnout
+5. Get a complete list of candidates that received votes
+6. Calculate the number of votes each candidate received
+7. Calculate the percentage of votes each candidate won
+8. Determine election winner
+
+## Resources
+- Data Source: election_results.csv
+- Software: Python 3.8.5, Visual Studio Code 1.56.2
+
+## Summary
+The resultant analysis of the election is as follows:
+- 369,711 total votes were cast in the election
+- The Counties that participated:
+  - Jefferson with 10.5% of the votes and 38,855 total voters
+  - Denver with 82.8% of the votes and 306,055 total voters
+  - Jefferson with 6.7% of the votes and 24,801 total voters
+- Largest Turnout was Denver
+- The candidates in the election were:
+  - Charles Casper Stockham
+  - Diana DeGette
+  - Raymon Anthony Doane
+- The candidate results were:
+  - Charles Casper Stockham received 23.0% of the votes and 85,213 total votes
+  - Diana DeGette received 73.8% of the votes and 272,892 total votes
+  - Raymon Anthony Doane received 3.1% of the votes and 11,606 total votes
+- The winner of the election was:
+  - Diana DeGette
